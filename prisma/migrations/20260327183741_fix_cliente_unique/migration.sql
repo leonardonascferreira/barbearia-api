8128b391d9280ade172265d61d31695e878a79e5
leonardonascferreira <leonardonascferreira@gmail.com>
2026-03-27 15:38:33 -0300

    corrige migrations

--- prisma/migrations/20260327183741_fix_cliente_unique/migration.sql
+++ prisma/migrations/20260327183741_fix_cliente_unique/migration.sql
@@ -1,11 +1,12 @@
 /*
   Warnings:
 
+  - A unique constraint covering the columns `[telefone]` on the table `Cliente` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[email]` on the table `Cliente` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropIndex
-DROP INDEX `Barbeiro_senha_key` ON `barbeiro`;
+-- CreateIndex
+CREATE UNIQUE INDEX `Cliente_telefone_key` ON `Cliente`(`telefone`);
 
 -- CreateIndex
 CREATE UNIQUE INDEX `Cliente_email_key` ON `Cliente`(`email`);
